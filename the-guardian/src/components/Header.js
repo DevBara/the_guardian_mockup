7c9a93a973a75b65c1bf277d516dd332e5eeaa1c
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-04 14:38:39 -0500

    styling:

--- the-guardian/src/components/Header.js
+++ the-guardian/src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 class Header extends React.Component {
     render(){
         return (
+            
             <header className="header">
-                <h1>Support the Guardian</h1>
-                <h2>Support the journalism with a year-end gift</h2>
+                <div>
+                    <h1>Support the Guardian</h1>
+                    <h2>Support the journalism with a year-end gift</h2>
+                </div>
+
+                <NavBar />
             </header>
+           
+                
+            
         )
        
     }
---
+++ the-guardian/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar(){
+    return(
+        <nav>
+            <ul>
+                <li>Search Jobs</li>
+                <li> Sign In</li>
+                <li> Search</li>
+            </ul>
+        </nav>
+
+    )
+    
+
+}
+
+export default NavBar
